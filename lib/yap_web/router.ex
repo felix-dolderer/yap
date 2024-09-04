13971c03569dcef934d28481d8612b37c9b2e625
Felix Dolderer <mail@felixdolderer.com>
2024-09-04 21:35:53 +0200

    CSP

--- lib/yap_web/router.ex
+++ lib/yap_web/router.ex
@@ -7,7 +7,7 @@ defmodule YapWeb.Router do
     plug :fetch_live_flash
     plug :put_root_layout, html: {YapWeb.Layouts, :root}
     plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug :put_secure_browser_headers, %{"content-security-policy" => "default-src 'self'"}
   end
 
   pipeline :api do
